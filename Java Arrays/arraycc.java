c687c141e0b4027843fa3cbb262a090580febf00
Xsidz <siddhesh.kabraa@gmail.com>
2025-03-29 17:31:35 +0530

    Day 5

--- Java Arrays/arraycc.java
+++ Java Arrays/arraycc.java
@@ -12,35 +12,71 @@ public class arraycc {
         }
     }
 
-    public static void Largestno(int arr[]){
+    public static void Largestno(int arr[]) {
         int large = 0;
         for (int i = 0; i < arr.length; i++) {
-            if(arr[i] > large){
+            if (arr[i] > large) {
                 large = arr[i];
             }
-            
+
         }
         System.out.println("The Largest number is : " + large);
     }
 
-    public static int BinarySearch(int arr[], int key){
-        int start =0 , end = arr.length-1;
-        while (start <= end) {
-            int mid = start+end/2;
+    public static void pairArray(int arr[]) {
+        int inx = 0;
+        while (inx < arr.length) {
+            for (int i = inx + 1; i < arr.length; i++) {
+                System.out.print("(" + arr[inx] + " " + arr[i] + ")");
+            }
+            inx++;
+            System.out.println();
+        }
+    }
 
-            if(arr[mid] == key){
+    public static int BinarySearch(int arr[], int key) {
+        int start = 0, end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + end / 2;
+
+            if (arr[mid] == key) {
                 return mid;
             }
-            if(arr[mid] < key){
-                start = mid+1;
-               
+            if (arr[mid] < key) {
+                start = mid + 1;
+
             }
-            if(arr[mid] > key){
-                end = mid-1;
+            if (arr[mid] > key) {
+                end = mid - 1;
             }
-            
+
         }
-       return -1;
+        return -1;
+    }
+
+    public static void ReverseArray(int arr[]) {
+        int front = 0, last = arr.length - 1;
+
+        while (front < last) {
+            int temp = arr[last];
+            arr[last] = arr[front];
+            arr[front] = temp;
+            front++;
+            last--;
+        }
+    }
+
+    public static void subarrays(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                for (int j2 = i; j2 <= j; j2++) {
+
+                    System.out.print(arr[j2]);
+
+                }
+                System.out.println();
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -63,5 +99,22 @@ public class arraycc {
         Largestno(num);
 
         System.out.println("The key was found at :" + BinarySearch(num, 6));
+
+        // resverse an array
+
+        int sum[] = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+        ReverseArray(sum);
+
+        for (int i = 0; i < sum.length; i++) {
+            System.out.print(sum[i] + " ");
+        }
+
+        // Pair Array
+        pairArray(sum);
+
+        // SubArrays
+
+        subarrays(sum);
+
     }
 }
---
+++ Java Arrays/tempCodeRunnerFile.java
@@ -0,0 +1,3 @@
+for (int i = 0; i < sum.length; i++) {
+        //     System.out.print(sum[i]+ " ");
+        // }
